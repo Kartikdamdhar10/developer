a7c72d7b40da7f4f5afdb6c5fa425336cfa07496
kdamdhar <kdamdhar@rediffmail.com>
2022-07-09 13:51:02 +0530

    new commit

---
+++ swap.py
@@ -0,0 +1,13 @@
+def swaplist(newlist):
+    size = len(newlist)
+
+    temp = newlist[0]
+    newlist[0] = newlist[size - 1]
+    newlist[size - 1] = temp
+
+    return newlist
+
+
+newlist = [12,35,89,76,20]
+
+print(swaplist(newlist))
---
+++ tuple.py
@@ -0,0 +1,21 @@
+mytuple = ('irish','phoeinix','chesus')
+print(mytuple)
+
+print(len(mytuple))
+
+zero = ('abcd',)
+print(zero)
+
+x = ('apple','banna','juice')
+y = list(x)
+y[1] = 'kiwi'
+x = tuple(y)
+print(x)
+
+thisisset = {'banana','apple','soc'}
+
+print(thisisset)
+
+thisisdict = {'branch':'mech','sub':'physics','marks':'40'}
+
+print(thisisdict['branch'])
